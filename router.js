82ce5c9b0c0489614105971c368d7cf674609168
mbortg995 <mbortg995@gmail.com>
2024-12-11 18:31:38 +0100

    fix: removed console.log as requested per Pepe

--- router.js
+++ router.js
@@ -5,7 +5,6 @@ import snippetsRoutes from "./snippets/snippets.router.js";
 const router = Router();
 
 router.get( '/', (req, res) => {
-  console.log('Esto es un get');
   res.send("Bienvenido a mi API");
 });
 
