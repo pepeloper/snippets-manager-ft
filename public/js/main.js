d7162d7924031d76dfac392650e1e7450ec99177
mbortg995 <mbortg995@gmail.com>
2024-12-13 20:04:11 +0100

    feat: list snippets on main page

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+const BASE_URL ='http://localhost:3000';
+let snippets = null;
+
+async function getSnippets(){
+  const response = await fetch(`${BASE_URL}/snippets`);
+  snippets = await response.json();
+}
+
+function createSnippetListItem(snippet, isActive = false) {
+  return `
+      <div class="snippet-item ${isActive ? 'active' : ''}"
+           data-id="${snippet.id}"
+           tabindex="0"
+           role="button"
+           aria-selected="${isActive}">
+          <div class="snippet-item-title-wrapper">
+              <img class="snippet-item-author-img" src="/images/${snippet.author.toLowerCase()}.png" alt="${snippet.author}">
+              <p class="snippet-item-title">${snippet.title}</p>
+          </div>
+          <div class="snippet-item-meta">
+              <span class="snippet-item-language">${snippet.category}</span>
+          </div>
+      </div>
+  `;
+}
+
+function renderSnippets (snippets){
+  const snippetList = document.querySelector(".snippets-list");
+  const list = snippets.map(snippet => createSnippetListItem(snippet));
+  snippetList.innerHTML = list.join('');
+}
+
+async function main() {
+  await getSnippets();
+  renderSnippets(snippets);
+}
+
+main();
